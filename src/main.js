a7a91b3ea2f2680d650bf6a7e980ce69b8adf9b9
farahsamaha <farahsamaha121@gmail.com>
2022-05-30 12:30:20 +0300

    initialize project with libraries

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
+import * as VueAos from 'vue-aos'
+import 'animate.css'
+import { ValidationProvider } from 'vee-validate'
 
+Vue.use(VueAos)
 Vue.config.productionTip = false
+Vue.component('ValidationProvider', ValidationProvider)
 
 new Vue({
+  router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
